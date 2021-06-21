09df11adf5fab30b29d1835b3bd4ce287c108b6d
Alexandra Hurst <82131699+AlNHurst@users.noreply.github.com>
2021-06-21 17:21:37 -0400

    added function to inner join employee.role_id and role.id

--- index.js
+++ index.js
@@ -8,16 +8,25 @@ const connection = mysql.createConnection({
     database: 'employee_db'
 });
 
+// function to view all employees 
+
 const viewEmployees = () => {
     console.log('Viewing all employees...\n');
-    connection.query('SELECT * FROM employees', (err, results) => {
+    let query = connection.query(`
+    SELECT employees.first_name, employees.last_name, employees.role_id, employees.manager_id, roles.title, roles.salary, roles.department_id
+    FROM employees
+    INNER JOIN roles
+    ON employees.role_id = roles.id`,
+    (err, results) => {
         if (err) throw (err);
         console.table(results);
         connection.end();
     });
 };
 
+
+
 connection.connect((err) => {
     if (err) throw err;
     viewEmployees();
-  });
+});
